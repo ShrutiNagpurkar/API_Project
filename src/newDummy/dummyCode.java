54bb87fe53c8c75a9c9e7765d82cf9ba036b4aea
shruti <nagpurkars05@gmail.com>
2024-06-08 02:23:07 +0530

    New commit from develop branch

--- src/newDummy/dummyCode.java
+++ src/newDummy/dummyCode.java
@@ -4,6 +4,7 @@ public class dummyCode {
 
 	public static void main(String[] args)
 	{
-		System.out.println("This is dummy code to push in the repository ");
+		System.out.println("This is dummy code to push in the repository");
+		System.out.println("to check this is added to develop");
 	}
 }
